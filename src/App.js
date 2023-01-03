30d9efdcd22e5157720bc89ea7f7bdbc41e54e06
kaigormandev <KaiGorman1023@outlook.com>
2023-01-03 00:11:08 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import DrawingArea from './DrawingArea';
+
+function App() {
+  return (
+    <>
+      <h1> Drawing Area</h1>
+      <div className="App drawing-area">
+          
+          <DrawingArea />
+      </div>
+    </>
+  );
+}
+
+export default App;
